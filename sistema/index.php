8edc92c004851e66df6150b80e6854b4ac816297
mani <maniortiz98@gmail.com>
2020-06-22 18:54:03 -0500

    23-fac

---
+++ sistema/index.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <?php include "includes/scripts.php" ?>
+    <title>Sistema</title>
+  </head>
+  <body>
+
+  <?php include "includes/header.php" ?>
+
+
+
+
+ 
+  <?php include "includes/footer.php" ?>
+   </body>
+</html>
